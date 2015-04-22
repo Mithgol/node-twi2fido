75480c2d9a93638fae07e8ddb4c7358213f33bb8
Mithgol the Webmaster <getgit@mithgol.ru>
2015-04-22 14:02:48 +0300

    use MomentJS, start writing code (console output and no lastread yet)

--- t2f-core.js
+++ t2f-core.js
@@ -1,1 +1,57 @@
-"use strict";
+var fs = require('fs');
+var path = require('path');
+var util = require('util');
+var moment = require('moment');
+var simteconf = require('simteconf');
+var twitter = require('twitter');
+
+var config = simteconf( path.join(__dirname, 'twi2fido.config') );
+
+module.exports = function(loginName, textOutput, fileLastRead){
+   textOutput   = path.resolve(__dirname, textOutput);
+   fileLastRead = path.resolve(__dirname, fileLastRead);
+
+   var twi = new twitter({
+      consumer_key:        config.last('ConsumerKey'),
+      consumer_secret:     config.last('ConsumerSecret'),
+      access_token_key:    config.last('AccessTokenKey'),
+      access_token_secret: config.last('AccessTokenSecret'),
+   });
+   twi.get(
+      'statuses/user_timeline',
+      {
+         // include_rts: false,
+         screen_name: loginName
+      },
+      function(err, tweetList){
+         if( err ) throw new Error( util.inspect(err, { depth: null }) );
+
+         var content = '';
+         // console.log( util.inspect(tweetList, { depth: null }) );
+         tweetList.forEach(function(tweet, idx){
+            if( idx > 0 ) content += '\n\n\n\n';
+            var source = tweet.retweeted_status || tweet;
+
+            content += [
+               source.user.name,
+               ' (@',
+               source.user.screen_name,
+               ') ',
+               // source.created_at,
+               // '\n',
+               moment(
+                  new Date(source.created_at)
+               ).utc().format('YYYY-MM-DD HH:mm:ss'),
+               ' (UTC)\n\n',
+               'https://twitter.com/',
+               source.user.screen_name,
+               '/status/',
+               source.id,
+               '\n\n',
+               source.text
+            ].join('');
+         });
+         console.log(content);
+      }
+   );
+};
--- twi2fido.js
+++ twi2fido.js
@@ -1,3 +1,37 @@
 #!/usr/bin/env node
 
-console.log('Hello world.');
+var twi2fido = require('./t2f-core.js');
+var clog = console.log;
+
+var loginName, textOutput, fileLastRead;
+
+if( process.argv.length < 3 ){
+   clog('Usage:');
+   clog('   twi2fido loginName textOutput fileLastRead');
+   clog('');
+   clog('Parameters:');
+   clog('');
+   clog('loginName    -- login name (screen name) of a microblog in Twitter');
+   clog('');
+   clog('textOutput   -- (optional) name of an output text file');
+   clog('                where the recent tweets should be written to.');
+   clog('                (By default, loginName.tweets.txt.)');
+   clog('');
+   clog('fileLastRead -- (optional) name of a file where the number');
+   clog('                of the last read tweet will be stored.');
+   clog('                (By default, loginName.lastread.txt.)');
+} else if (process.argv.length == 3) {
+   loginName    = process.argv[2];
+   textOutput   = loginName + '.tweets.txt';
+   fileLastRead = loginName + '.lastread.txt';
+} else if (process.argv.length == 4) {
+   loginName    = process.argv[2];
+   textOutput   = process.argv[3];
+   fileLastRead = loginName + '.lastread.txt';
+} else if (process.argv.length > 4) {
+   loginName    = process.argv[2];
+   textOutput   = process.argv[3];
+   fileLastRead = process.argv[4];
+}
+
+twi2fido(loginName, textOutput, fileLastRead);
